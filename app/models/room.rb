702f77794764463f60c724fa5d5a9f0711b1894a
Barnett Klane <blklane@syr.edu>
2014-08-26 16:35:09 -0700

    fix scope uniquness for rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :user
-  validates :href, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, allow_nil: true
+  validates :href,  uniqueness: { scope: :user_id }, presence: true
+  validates :email, uniqueness: { scope: :user_id }, presence: true, allow_nil: true
   scope :need_emails, -> { where(emailed_at: nil).where('email IS NOT NULL') }
 end

4c4f8de0577deccefe941a9e470c346161abce34
Barnett Klane <blklane@syr.edu>
2014-08-26 09:09:23 -0700

    mechanize is a fucker

--- app/models/craig_scraper.rb
+++ app/models/craig_scraper.rb
@@ -5,14 +5,13 @@ class CraigScraper
   def initialize(user_id)
     @logger = Logger.new('room_finder.log')
     @user   = User.find(user_id)
+    @agent  = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
   end
 
   def find_rooms
     type = @user.housing_type
-    # Create a Mechanize agent
-    a = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
 
-    a.get(@user.url) do |page|
+    @agent.get(@user.url) do |page|
 
       doc = page.parser
 

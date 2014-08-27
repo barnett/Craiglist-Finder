4a99e3d60b789baf4b4a6c817b00cbd45c286219
Barnett Klane <blklane@syr.edu>
2014-08-26 18:37:27 -0700

    handling for other locations

--- app/models/craig_scraper.rb
+++ app/models/craig_scraper.rb
@@ -7,21 +7,25 @@ class CraigScraper
     @user   = User.find(user_id)
   end
 
+  # iterate through each link
   def find_rooms
-    # iterate through each link
     scrape_links.each { |link| create_room(link) }
   end
 
   private
 
+  def cl_city
+    @user.url.match(/search\/(?<city>.*)\//)['city']
+  end
+
   def create_room(post_link)
     # Create an Room check if you've already contacted it using ActiveRecord create
-    href = "http://sfbay.craigslist.org#{post_link}"
+    href = "http://#{cl_city}.craigslist.org#{post_link}"
     room = @user.rooms.new(href: href)
 
     if room.valid?
       contact_url = construct_contact_url(room.href)
-      room.email = parse_email(contact_url).try(:strip)
+      room.email  = parse_email(contact_url).try(:strip)
       room.save
     else
       @logger.info("Already contacted #{href}")
@@ -33,23 +37,23 @@ class CraigScraper
 
   # Construct the contact page url
   def construct_contact_url(href)
-    base_url = href.slice(/\A.+\.org/)
+    base_url   = href.slice(/\A.+\.org/)
     listing_id = href.slice(/\d+/)
     base_url + "/reply/" + listing_id
   end
 
   def scrape_links
     agent = Mechanize.new do |agent|
-      agent.log = @logger
+      agent.log              = @logger
       agent.user_agent_alias = 'Mac Safari'
-      agent.robots = false
+      agent.robots           = false
     end
 
     page = agent.get(@user.url)
 
     page.parser.css("a")
       .map { |link| link["href"] }
-      .select { |link| link.match(/\/sfc\/#{@user.housing_type}\/\d+/) }
+      .select { |link| link.match(/\/#{cl_city}\/#{@user.housing_type}\/\d+/) }
       .uniq!
 
   rescue Mechanize::ResponseCodeError => exception
@@ -61,14 +65,14 @@ class CraigScraper
     retries = 0
     begin
       xml_elems = Nokogiri::HTML(open(contact_url))
-      email = xml_elems.css("body > div > ul:nth-child(4) > li > a")[0]
+      email     = xml_elems.css("body > div > ul:nth-child(4) > li > a")[0]
       email.try(:text)
 
     rescue Exception=>e
       puts "ERROR: #{e}"
       retries += 1
-      sleep SLEEP_ON_ERROR
-      retry unless retries >= RETRY_COUNT
+      sleep 5
+      retry unless retries >= 3
     end
   end
 
